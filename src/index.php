2b26456384408fb7cd852fc6097ddd11d9a5981d
Berkan Ümütlü <berkanumutlu1@gmail.com>
2024-08-13 17:35:06 +0300

    Added index card

---
+++ src/index.php
@@ -0,0 +1,43 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Web Tools</title>
+    <meta name="description" content="Web Tools by Berkan Ümütlü">
+    <meta name="keywords" content="php, web, web tools, php web tools">
+    <meta name="author" content="Berkan Ümütlü">
+    <meta name="copyright" content="Berkan Ümütlü">
+    <meta name="owner" content="Berkan Ümütlü">
+    <meta name="url" content="https://github.com/berkanumutlu">
+    <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/web/css/style.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-xl-8 mx-auto">
+            <div class="card my-5">
+                <div class="card-header d-flex align-items-center">
+                    <img src="assets/web/images/www.png" class="me-2" width="32" height="32" alt="Web Tools Logo">
+                    <h1 class="mb-0 fs-4 fw-semibold">Web Tools</h1>
+                </div>
+                <div class="card-body">
+
+                </div>
+                <div class="card-footer text-body-secondary">
+                    <p class="mb-0">Copyright © 2023
+                        <a href="https://github.com/berkanumutlu" target="_blank">Berkan Ümütlü</a>. All Right Reserved.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="assets/plugins/jquery/jquery-3.7.1.min.js"></script>
+<script src="assets/web/js/main.js"></script>
+</body>
+</html>
